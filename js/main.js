8adad23f88fa9999db0fd0485ffd7b24363a1ba1
丁平 <1572804782@qq.com>
2019-02-28 14:24:37 +0800

    更改主题

--- js/main.js
+++ js/main.js
@@ -1,57 +1,38 @@
-/**
- * some JavaScript code for this blog theme
- */
-/* jshint asi:true */
+---
+layout: null
+sitemap:
+  exclude: 'yes'
+---
 
-/////////////////////////header////////////////////////////
-/**
- * clickMenu
- */
-(function() {
-  if (window.innerWidth <= 770) {
-    var menuBtn = document.querySelector('#headerMenu')
-    var nav = document.querySelector('#headerNav')
-    menuBtn.onclick = function(e) {
-      e.stopPropagation()
-      if (menuBtn.classList.contains('active')) {
-        menuBtn.classList.remove('active')
-        nav.classList.remove('nav-show')
-      } else {
-        nav.classList.add('nav-show')
-        menuBtn.classList.add('active')
-      }
-    }
-    document.querySelector('body').addEventListener('click', function() {
-      nav.classList.remove('nav-show')
-      menuBtn.classList.remove('active')
-    })
-  }
-}());
-
-//////////////////////////back to top////////////////////////////
-(function() {
-  var backToTop = document.querySelector('.back-to-top')
-  var backToTopA = document.querySelector('.back-to-top a')
-  // console.log(backToTop);
-  window.addEventListener('scroll', function() {
-
-    // 页面顶部滚进去的距离
-    var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
-
-    if (scrollTop > 200) {
-      backToTop.classList.add('back-to-top-show')
+$(document).ready(function () {
+  $('a.blog-button').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
     } else {
-      backToTop.classList.remove('back-to-top-show')
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
 
-  // backToTopA.addEventListener('click',function (e) {
-  //     e.preventDefault()
-  //     window.scrollTo(0,0)
-  // })
-}());
+  if (window.location.hash && window.location.hash == '#blog') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
 
-//////////////////////////hover on demo//////////////////////////////
-(function() {
-  var demoItems = document.querySelectorAll('.grid-item')
-}());
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+  $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+})
